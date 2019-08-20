7607ea86b910fcaad5042cb5cad9b4fbde4647eb
王美建 <wangmeijian@360.net>
2019-08-21 00:58:58 +0800

    改为正式环境提测

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,13 +1,15 @@
 //app.js
 App({
   onLaunch: function () {
+    const releaseEnv = 'game-pcm9t';
+    const developEnv = 'test-7w5bo';
     
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'game-pcm9t',
-        // env: 'test-7w5bo',
+        env: releaseEnv,
+        // env: developEnv,
         traceUser: true
       })
     }
