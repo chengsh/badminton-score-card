b85578204f5f20117885b119b6f6395ed026515e
wangmeijian <wangmeijian@qianxin.com>
2019-09-25 14:25:45 +0800

    国际赛事列表

--- miniprogram/pages/tournament/index.js
+++ miniprogram/pages/tournament/index.js
@@ -22,6 +22,14 @@ Page({
       title: `${this.data.year}年国际羽毛球赛事`
     }
   },
+  dateFormat(start, end){
+    let startDate = new Date(start);
+    let endDate = new Date(end);
+    let startFormat = startDate.getMonth() + 1 + '月' + startDate.getDate();
+    let endFormat = endDate.getMonth() + 1 + '月' + endDate.getDate();
+
+    return startFormat + '-' + endFormat;
+  },
   getTournament(){
     let year = this.data.year;
     let setData = result => {
@@ -43,8 +51,11 @@ Page({
       }
     }).then(res => {
       let result = res.data.length > 0 ? res.data : [];
+      result = result.map(item => {
+        item.date = this.dateFormat( item['start-date'],item['end-date'] );
+        return item;
+      })
       this.data.cache[year] = result;
-      console.log( result );
       setData(result);
     })
   }
